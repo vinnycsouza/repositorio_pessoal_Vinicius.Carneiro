f8ef29353ed6472baa236ca9ac547310febb1a8c
Vinicius Carneiro Souza e Silva <vinnycsouza93@gmail.com>
2026-01-05 08:38:28 -0300

    atualização de arquivo

---
+++ estrutura_repeticao_for.py
@@ -0,0 +1,17 @@
+texto = input("Informe um texto: ")
+VOGAIS = "AEIOU"
+
+
+# exemplo utilizando um iterável
+for letra in texto:
+    if letra.upper() in VOGAIS:
+        print(letra, end="")
+else:
+     print()  
+      
+
+
+# exemplo utilizando a função bulit-in range
+for numero in range(0, 51, 5):
+    print(numero, end="")
+
