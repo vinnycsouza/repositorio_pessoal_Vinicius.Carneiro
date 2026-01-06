e645f0f3bcbb80f19ef77afaa8eab1762cb5db22
Vinicius Carneiro Souza e Silva <vinnycsouza93@gmail.com>
2026-01-06 08:35:26 -0300

    atalização de projeto

--- Codigos_py_DIO/18_sorted.py
+++ Codigos_py_DIO/18_sorted.py
@@ -2,3 +2,5 @@ linguagens = ["python", "js", "c", "java", "csharp"]
 
 print(sorted(linguagens, key=lambda x: len(x)))  # ["c", "js", "java", "python", "csharp"]
 print(sorted(linguagens, key=lambda x: len(x), reverse=True))  # ["python", "csharp", "java", "js", "c"]
+
+print(sorted(linguagens))
