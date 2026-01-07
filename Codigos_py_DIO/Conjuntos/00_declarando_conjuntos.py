d34e3475cecd4e6dc78722887ec625017064505f
Vinicius Carneiro Souza e Silva <vinnycsouza93@gmail.com>
2026-01-06 21:08:27 -0300

    atualização de projeto

--- Codigos_py_DIO/Conjuntos/00_declarando_conjuntos.py
+++ Codigos_py_DIO/Conjuntos/00_declarando_conjuntos.py
@@ -6,3 +6,8 @@ print(letras)  # {"b", "a", "c", "x", "i"}
 
 carros = set(("palio", "gol", "celta", "palio"))
 print(carros)  # {"gol", "celta", "palio"}
+
+
+
+linguagens = {"python", "java", "python"}
+print(linguagens)
