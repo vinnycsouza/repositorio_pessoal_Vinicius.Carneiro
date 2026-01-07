f8e19f27072efebc0d43bd0c06c3a58409d05c06
Vinicius Carneiro Souza e Silva <vinnycsouza93@gmail.com>
2026-01-07 20:40:06 -0300

    atualização de arquivo

--- Codigos_py_DIO/Funções/06_objetos_de_primeira_classe.py
+++ Codigos_py_DIO/Funções/06_objetos_de_primeira_classe.py
@@ -1,10 +1,24 @@
 def somar(a, b):
     return a + b
 
+def subtrair(a, b):
+    return a - b
+
+def test(a, b):
+    return a * 2 + b * 3
+
+
 
 def exibir_resultado(a, b, funcao):
     resultado = funcao(a, b)
-    print(f"O resultado da operação {a} + {b} = {resultado}")
+    print(f"O resultado da operação é de  = {resultado}")
 
 
 exibir_resultado(10, 10, somar)  # O resultado da operação 10 + 10 = 20
+exibir_resultado(10, 10, subtrair)
+exibir_resultado(10, 10, test)
+
+
+op = somar
+
+print(op(1,23))
--- Codigos_py_DIO/Funções/07_escopo_local_e_global.py
+++ Codigos_py_DIO/Funções/07_escopo_local_e_global.py
@@ -1,10 +1,18 @@
 salario = 2000
 
 
-def salario_bonus(bonus):
+def salario_bonus(bonus, lista):
     global salario
+
+    lista_aux = lista.copy()
+    lista_aux.append(2)  
+    print(f"lista aux={lista_aux}")    
+
     salario += bonus
     return salario
 
 
-salario_bonus(500)  # 2500
+lista = [1]
+salario_com_bonus = salario_bonus(500, lista)  # 2500
+print(salario_com_bonus)
+print(lista)
