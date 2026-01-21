f40fd36dfe2c288d3d1f72b90fcad8e6a930312f
Vinicius Carneiro Souza e Silva <vinnycsouza93@gmail.com>
2026-01-21 09:53:52 -0300

    atualização de projeto

--- Meus estudos PY/matplotlib.py
+++ Meus estudos PY/matplotlib.py
@@ -1,9 +1,14 @@
- import matplotlib.pyplot as plt
-
-from sklearn.datasets import make_regression
-x, y = make_regression(n_samples=200, n_features=1, noise=30)
 import matplotlib.pyplot as plt
 
+# Dados de exemplo
+x = [1, 2, 3, 4]
+y = [10, 20, 25, 30]
 
-plt.scatter(x,y)
+# Criar o gráfico
+plt.plot(x, y)
+plt.title("Meu Primeiro Gráfico")
+plt.xlabel("Eixo X")
+plt.ylabel("Eixo Y")
+
+# Mostrar o gráfico
 plt.show()
