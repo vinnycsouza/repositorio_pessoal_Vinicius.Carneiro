83daf3f7b1d2795a6e662b8beb29141f137c1e9b
Vinicius Carneiro Souza e Silva <vinnycsouza93@gmail.com>
2026-01-23 21:30:57 -0300

    atualização de projeto

--- etl_creditos/main.py
+++ etl_creditos/main.py
@@ -1,9 +1,9 @@
-from etl_creditos.extract import extract_csv
-from etl_creditos.transform import transform_empresas
-from etl_creditos.load import load_mensagens
+from extract import extract_csv
+from transform import transform_empresas
+from load import load_mensagens
 
 def main():
-    df = extract_csv("lista_empresa.csv")
+    df = extract_csv("lista_empresas.csv")
     mensagens = transform_empresas(df)
     load_mensagens(mensagens)
 
